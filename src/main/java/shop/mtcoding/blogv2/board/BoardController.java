bebb0a9c1cdc5b9d26e48423ce63724d96160dc0
jwy4285 <jwy4285@naver.com>
2023-08-11 14:04:30 +0900

    게시글쓰기 완료

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,11 +1,17 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import shop.mtcoding.blogv2.user.User;
+
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardRepository boardRepository;
 
     @GetMapping("/board/saveForm")
     public String saveForm() {
@@ -13,14 +19,18 @@ public class BoardController {
     }
 
     // 1. 데이터 받기
-    // 2. 인증체크
-    // 3. 유효성 검사
+    // 2. 인증체크 (:TODO)
+    // 3. 유효성 검사(:TODO)
     // 4. 핵심로직 호출(서비스) 비지니스로직이나 핵심로직을 서비스에서 트랜잭션 관리함
     // 5. view or data 응답 (뷰가될수도 있고 데이터가 될수도 있음)
     @PostMapping("/board/save")
     public String save(BoardRequest.SaveDTO saveDTO) {
-        System.out.println("title : " +saveDTO.getTitle());
-        System.out.println("content : " +saveDTO.getContent());
+        Board board = Board.builder()
+                .title(saveDTO.getTitle())
+                .content(saveDTO.getContent())
+                .user(User.builder().id(1).build())
+                .build();
+
         return "redirect:/";
     }
 
--- src/main/java/shop/mtcoding/blogv2/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardRepository.java
@@ -1,8 +1,12 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class BoardRepository {
+/*
+ * save()인서트, findById()프라이머리키 조회, findAll() 전체조회, count() 갯수셀때, deleteById()
+ * CRUD중에 업데이트만 빼고 다 있음 함수를 만들 필요가없음 CRUD 메서드를 주는애
+ */
+public interface BoardRepository extends JpaRepository<Board, Integer> {
 
 }
