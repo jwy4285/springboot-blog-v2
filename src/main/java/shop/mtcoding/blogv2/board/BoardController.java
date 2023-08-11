233904a01c47fcdea55b34bea13b4a1ad848fa7e
jwy4285 <jwy4285@naver.com>
2023-08-11 11:59:08 +0900

    기능정리 완료

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,8 +1,27 @@
 package shop.mtcoding.blogv2.board;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
+    // 1. 데이터 받기
+    // 2. 인증체크
+    // 3. 유효성 검사
+    // 4. 핵심로직 호출(서비스) 비지니스로직이나 핵심로직을 서비스에서 트랜잭션 관리함
+    // 5. view or data 응답 (뷰가될수도 있고 데이터가 될수도 있음)
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO saveDTO) {
+        System.out.println("title : " +saveDTO.getTitle());
+        System.out.println("content : " +saveDTO.getContent());
+        return "redirect:/";
+    }
 
 }
---
+++ src/main/java/shop/mtcoding/blogv2/board/BoardRequest.java
@@ -0,0 +1,16 @@
+package shop.mtcoding.blogv2.board;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class BoardRequest {
+
+    // 클래스에 내부 클래스 만들어서 관리
+    @Getter
+    @Setter
+    public static class SaveDTO {
+        private String title;
+        private String content;
+
+    }
+}
